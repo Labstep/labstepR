76953dfc1866c5ad1d618e861269ec9296c7ca5c
Barney Walker <barneywalker@Jamess-MacBook-Pro-2.local>
2019-03-19 18:04:08 +0000

    attach protocols at experiment creation

--- R/createExperiment.R
+++ R/createExperiment.R
@@ -4,8 +4,9 @@
 #' @name createExperiment
 #' @param user A labstep user object. Must contain an `api_key` field. Returned from `authenticate` command
 #' @param name The name you want to give your experiment.
+#' @param protocolIds List of ids for protocols to attach to the experiment.
 #' @param description Give your experiment a description to help you remember what it was about.
-#' @return Returns an `experiment` object 
+#' @return Returns an `experiment` object
 #' @export
 #' @examples
 #' user <- authenticate("demo@labstep.com","demopassword")
@@ -15,12 +16,30 @@
 
 library(httr)
 
-createExperiment <- function(user,name,description){
-  
+createExperiment <- function(user,name,description=NULL,protocolIds=NULL){
+
   req <- POST('https://api.labstep.com/api/generic/experiment-workflow',
               body=list(name=name,description=description),
               add_headers(apikey=user$api_key),
               encode='json')
-  
-  return(content(req))
+
+  experiment = content(req)
+
+  if(!is.null(protocolIds)){
+    for(id in protocolIds){
+      req <- POST('https://api.labstep.com/api/generic/experiment',
+                  body=list(experiment_workflow_id=experiment$id,protocol_id=id),
+                  add_headers(apikey=user$api_key),
+                  encode='json')
+    }
+
+    url = paste('https://api.labstep.com/api/generic/experiment-workflow/',experiment$id,sep='')
+    req <- GET(url,
+                add_headers(apikey=user$api_key),
+                encode='json')
+    experiment = content(req)
+
+  }
+
+  return(experiment)
 }
